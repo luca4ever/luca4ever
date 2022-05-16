fd42894817b5431e8930aaf68c5d2c6d067a7a60
luciano <developer.lfc@outlook.com.br>
2022-05-16 09:51:22 -0300

    Initial Code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='toolbar'>
+                <div>
+                    <a href='/'>MOTIVEAÇÃO-PALESTRAS</a>
+                </div>
+                <div>
+                <button>New Post</button>
+                <span>img1</span>
+                <span>img2</span>
+                </div>
+            </div>
+            </header>
+    );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './components/Header';
+import './styles.css';
+
+function Home() {
+    return (
+        <div>
+            <header></header></head>
+            <main className="main">CRM
+            <div className="navbar">navbar</div>
+            <div className="feed">feed</div>
+            </main>
+        </div>
+    );
+}
+
+export default Home;
